04107a1de84512a119bfc94d961b2ae56f1349a3
jerald <sanjer.socime@outlook.com>
2025-06-16 23:06:48 +0800

    nyahaha upload

---
+++ app/Config/Routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Hello::index');  // This replaces the default Welcome controller
+
---
+++ app/Views/hello_world.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello, World from CodeIgniter 4!</h1>
+</body>
+</html>
